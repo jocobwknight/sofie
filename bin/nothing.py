a77179879c30af540bbf7b7fd9b84f1d35e0fa57
Jocob <44737491+jocobwknight@users.noreply.github.com>
2021-11-04 02:01:55 -0800

    built in python3 with much better features/speed
    This is now pretty much an SDK in and of itself. It has the ability to be any kind of command dynamically (decided during search time). It parses the command arguments into a globally available dictionary (see line 98) for structure. It actively converts MV fields into lists and back to Splunk appropriate MV syntax on the way back, and it can change field names/order mid-processing. But it also has the ability to turn those extra processes off to make it much faster. Also for speed, it pipes the body from Splunk directly into a csv writer as it processes each row (unless field name alterations are enabled). I'd love to put it straight into stdin, but you have to preface your chunk with the body length, which is only known after you get done processing your chunk. It also has (albeit dry and untested) methods for generating new events, and it will decide on either generate or processing rows based on the settings.
    I still need to annotate it, but I also wanted to say that I tried recreating this script in C# and I couldn't get it to run significantly faster than python. Because Python is so intimately connected to Splunk, I decided beforehand that I would only change the language if I could get about a 50% drop in runtime. I may try some other languages down the line, but I have pretty high regard for C#'s performance, so I'm not holding out much hope. To be fair, though, I was only testing based on the stdin/stdout cycle between this script's process and Splunk's engine. Other languages might realize much more appealing performance boosts when it comes to the actual row-by-row data processing. (Especially when you account for Python's limitations with threading) So I wouldn't say it's completely over for other languages...

--- bin/nothing.py
+++ bin/nothing.py
@@ -1,149 +1,234 @@
-from __future__ import print_function, unicode_literals
-import os
-import sys
 import csv
 import json
-from datetime import datetime
-from collections import OrderedDict
+from os import sep
+import sys
+import uuid
+from io import StringIO
 
-"""
-What? This script can handle Splunk Custom Search Commands (V2) with only built-ins, and in a single 150-LOC script
+SETTINGS = {
+    # 'distributed': False,                            # (Not actually used, despite docs)
+    'generating': (True, False)[1],                    # (True generates, False modifies)
+    # 'maxinputs': six.MAXSIZE,                        # (Not actually used, despite docs)
+    # 'required_fields': ['field1'],                   # (Fields to return from remotes)
+    # 'run_in_preview': True,                          # (Not actually used, despite docs)
+    # 'streaming_preop': '| makeresults',              # (Filters data from remote searchers)
+    'type': ('events', 'reporting', 'streaming')[0],   # (events run local, streaming remote)
 
-Why?  1.) Squeezing any bit of performance I can; 2.) Low-level control; 3.) Because it's cool; 4.) To understand it
+    'can_alter_field_names': True,
+    'use_multivalue_fields': True
+}
 
-How?  Read on! It's essentially just string manipulation and stdin/stdout transactions with the splunkd process
-"""
-
-LOGS = os.path.abspath(__file__) + '.log'
-"""{
-    "action": "getinfo",
-    "preview": false,
-    "streaming_command_will_restart": false,
-    "searchinfo": {
-        "args": ["take my", "breath=away"],
-        "raw_args": ["\"take my\"", "breath=away"],
-        "dispatch_dir": "...",
-        "sid": "...",
-        "app": "custom",
-        "owner": "them",
-        "username": "you",
-        "session_key": "...",
-        "splunkd_uri": "https://127.0.0.1:8089",
-        "splunk_version": "8.0.5",
-        "search": "...",
-        "command": "custom",
-        "maxresultrows": 0,
-        "earliest_time": "0",
-        "latest_time": "0"
-    }
-}"""
-META = {}  # Sample above
-BODY = []  # Comes in as CSV, but converted to list of OrderedDict objects for each record
-EXECUTING, EXECUTED = {'finished': False}, {'finished': True}
+SEARCH_INFO = {}
+COMMAND_ARGS = None
+FIELD_ORDER = []
+RUNID = uuid.uuid4().int & (1<<64)-1
+DL = "_ri%d" % (RUNID % 100 + 100)
+SLASH, QUOTE, SPACE, EQUAL, VALUE, NAMED, GROUP, MULTI = DL + "l_", DL + "q_", DL + "s_", DL + "e_", DL + "v_", DL + "n_", DL + "g_", DL + "m_"
+TRUES =  [ 'TRUE',  'T', 'YES', 'Y', 'YEP' ]
+FALSES = [ 'FALSE', 'F', 'NO',  'N', 'NOPE' ]
 
 
-if sys.version_info[0] == 2:  # TODO: Need to test this on Python3
-    from io import BytesIO
-    def hear(i, s): return i.read(s)
-    def tell(i, c): i.write(c)
-    def hold(): return BytesIO()
-else:
-    from io import StringIO
-    def hear(i, s): return i.buffer.read(s).decode('utf-8')
-    def tell(i, c): i.buffer.write(bytes(c, encoding='utf-8'))
-    def hold(): return StringIO()
+class SplunkCSV(csv.Dialect):
+    delimiter = ','
+    doublequote = True
+    lineterminator = '\n' if sys.platform == 'win32' else '\r\n'
+    quotechar = '"'
+    quoting = csv.QUOTE_MINIMAL
+    skipinitialspace = False
 
 
-# TODO: Pretty much the cheapest way to log. You could log directly into Splunk, or you just give up on self-respect
-def log(it, nw=False):
-    it = str(it)
-    with open(LOGS, ('w' if nw else 'a')) as a:
-        a.write(str(datetime.now()) + ' ' + it + ('' if it.endswith('\n') else '\n'))
+def reverse_replace(reversed):
+    return reversed.replace(EQUAL, "=").replace(SPACE, " ").replace(QUOTE, "\"").replace(SLASH, "\\")
 
 
-# TODO: Read directly into the csv.reader so it's not storing the data twice
-def order(c):
-    full = list(csv.reader(c.splitlines()))
-    return [OrderedDict(zip(full[0], r)) for r in full[1:]]
+def args_to_dict(rawrgs, array_duplicates=True):
+    cleaned = []
+    for dirty in rawrgs:
+        clean = dirty
+        clean = clean.replace("\\\\", SLASH).replace("\\\"", QUOTE).replace(" ", SPACE)
+        eq_in = False
+        for eq in ["\"=\"", "\"=", "=\"", "="]:
+            if eq in clean:
+                eq_in = True
+                cleaner = clean.split(eq, 1)
+                if len(cleaner[0]) > 0:
+                    cleaned.append(cleaner[0].replace("\"", "").replace("=", EQUAL))
+                cleaned.append("=")
+                if len(cleaner[1]) > 0:
+                    cleaned.append(cleaner[1].replace("\"", "").replace("=", EQUAL))
+        if not eq_in:
+            cleaned.append(clean)
+    
+    fields, equals, as_map, by_map, was_by = [], {}, {}, {}, False
+    joined = ' '.join(cleaned).replace(" = ", VALUE).replace(" as ", NAMED).replace(" aS ", NAMED).replace(" As ", NAMED).replace(" AS ", NAMED)
+    separated = joined.split(' ')
+
+    for separate in separated:
+        if VALUE in separate:
+            left_right = [reverse_replace(side) for side in separate.split(NAMED)]
+            right_upper = left_right[1].upper()
+            if right_upper in TRUES or right_upper in FALSES:
+                left_right[1] = right_upper in TRUES
+            else:
+                try:
+                    left_right[1] = int(left_right[1])
+                except ValueError:
+                    try:
+                        left_right[1] = float(left_right[1])
+                    except ValueError:
+                        pass
+            if left_right[0] in equals and array_duplicates:
+                equals[left_right[0]].append(left_right[1])
+            elif left_right[0] not in equals and array_duplicates:
+                equals[left_right[0]] = [left_right[1]]
+            else:
+                equals[left_right[0]] = left_right[1]
+        elif NAMED in separate:
+            left_right = [reverse_replace(side) for side in separate.split(NAMED)]
+            if left_right[0] != '':
+                fields.append(left_right[0])
+            as_map[left_right[0]] = left_right[1]
+            by_map[left_right[0]] = was_by
+        elif separate.lower() == 'by':
+            was_by = True
+        elif separate != '':
+            field = reverse_replace(separate)
+            fields.append(field)
+            by_map[field] = was_by
+    return { 'originally': rawrgs, 'key_values': equals, 'fieldnames': fields, 'as_aliases': as_map, 'grouped_by': by_map }
 
 
-# TODO: Read the meta directly with json.load so it's not storing twice (like the body)
-def read(i):
-    header = i.readline()
-    parts = header.split(',')  # Header will be: chunked 1.0,{int: meta length},{int: body length}
-    meta_len, body_len = int(parts[1]), int(parts[2])  # IMPORTANT: these lengths are character count, not bytes
-    meta = json.loads(hear(i, meta_len))
-    if body_len > 0:
-        body = order(hear(i, body_len))
-    else:
-        body = []
-    return meta, body
+def mv_row_handler(row, incoming=True):
+    global FIELD_ORDER
+    if not SETTINGS['use_multivalue_fields']:
+        return row
+    for field in row.keys():
+        if field.startswith('__mv_') and incoming and row[field] != '':
+            true_field = field.replace('__mv_', '', 1)
+            mv_field = row[field].replace('$$', MULTI)[1:-1]
+            mv_list = [mv_item.replace(MULTI, '$') for mv_item in filter(lambda x: x != '', mv_field.split('$;$'))]
+            row[true_field] = mv_list
+        else:
+            if field not in FIELD_ORDER:
+                FIELD_ORDER.append(field)
+            if not incoming and isinstance(row[field], list):
+                other_field = '__mv_%s' % field
+                if len(row[field]) > 0:
+                    mv_list = [mv_item.replace('$', "$$") for mv_item in row[field]]
+                    mv_field = ('$%s$' if len(mv_list) > 0 else '%s') % '$;$'.join(mv_list)
+                    row[field] = ' '.join(row[field])
+                    row[other_field] = mv_field
+                else:
+                    row[field] = ''
+                    row[other_field] = ''
+    return row
 
 
-# TODO: Same thing, write to json.dump / csv.writer directly. Avoided because of PY2/3 buffering differences but...
-def write(o, m, b):
-    m = json.dumps(m)
-    p = hold()
-    w = csv.writer(p)
-    if len(b) > 0:
-        w.writerow(list(b[0].keys()))
-        for r in b:
-            v = []
-            for j, u in r.items():
-                v.append(u)
-            w.writerow(v)
-        b = p.getvalue()
-    else:
-        b = ''
-    h = 'chunked 1.0,%d,%d' % (len(m), len(b))
-    tell(o, '\n'.join((h, m, b) if len(b) > 0 else (h, m)))
-    o.flush()
+def main(in_stream, out_stream):
+    global COMMAND_ARGS
+    global SEARCH_INFO
+    global SETTINGS
+    repeat = True
+    while repeat:
+        chunk_header = in_stream.readline()
+        if len(chunk_header) == 0:
+            break
+        lengths = [int(num) for num in chunk_header.rstrip('\n').split(',')[1:]]
+
+        metadata = in_stream.read(lengths[0])
+        loaded_metadata = json.loads(metadata)
+        if loaded_metadata['action'] == 'getinfo':
+            SEARCH_INFO = loaded_metadata
+            if 'searchinfo' in loaded_metadata and 'raw_args' in loaded_metadata['searchinfo'] and COMMAND_ARGS is None:
+                COMMAND_ARGS = args_to_dict(loaded_metadata['searchinfo']['raw_args'])
+                print(json.dumps(COMMAND_ARGS, indent=4), file=sys.stderr)
+            if len(COMMAND_ARGS['fieldnames']) > 0:
+                if COMMAND_ARGS['fieldnames'][0] == '+gen':
+                    SETTINGS['generating'] = True
+                    COMMAND_ARGS['fieldnames'] = COMMAND_ARGS['fieldnames'][1:]
+                if COMMAND_ARGS['fieldnames'][0] == '-gen':
+                    SETTINGS['generating'] = False
+                    COMMAND_ARGS['fieldnames'] = COMMAND_ARGS['fieldnames'][1:]
+            return_header = {
+                'type': SETTINGS['type'],
+                'generating': SETTINGS['generating']
+            }
+            if 'required_fields' in SETTINGS and SETTINGS['type'] == 'streaming':
+                return_header['required_fields'] = SETTINGS['required_fields']
+            if 'streaming_preop' in SETTINGS and SETTINGS['type'] == 'streaming':
+                return_header['streaming_preop'] = SETTINGS['streaming_preop']
+            output = ''
+        else:
+            if not loaded_metadata['finished']:
+                return_header = {'finished': False}
+            else:
+                return_header = {'finished': True}
+                repeat = False
+
+            if lengths[1] == 0 and SETTINGS['generating']:
+                create = StringIO()
+                fields = generate_fieldnames()
+                writer = csv.DictWriter(create, fieldnames=fields, dialect=SplunkCSV)
+                writer.writeheader()
+                for row in generate_rows():
+                    writer.writerow(row)
+                output = create.getvalue()
+            else:
+                events = in_stream.read(lengths[1])
+                table = csv.DictReader(StringIO(events), dialect=SplunkCSV)
+                alters = StringIO()
+                if SETTINGS['can_alter_field_names']:
+                    inspect_fieldnames(table.fieldnames)
+                    result = alter_rows(table)
+                    fields = FIELD_ORDER + ['__mv_%s' % field for field in FIELD_ORDER]
+                    writer = csv.DictWriter(alters, fieldnames=fields, dialect=SplunkCSV)
+                    writer.writeheader()
+                    writer.writerows(result)
+                else:
+                    inspect_fieldnames(table.fieldnames)
+                    fields = FIELD_ORDER + ['__mv_%s' % field for field in FIELD_ORDER]
+                    writer = csv.DictWriter(alters, fieldnames=fields, dialect=SplunkCSV)
+                    writer.writeheader()
+                    alter_rows(table, writer)
+                output = alters.getvalue()
+
+        return_header = json.dumps(return_header)
+        toit = 'chunked 1.0,%d,%d\n%s\n%s' % (len(return_header), len(output), return_header, output)
+        out_stream.write(toit)
+        out_stream.flush()
 
 
-# TODO: Despite the claims, Splunk fires up the process several times. It would be nice to cache the meta on sid...
-def getinfo(meta):
-    global META
-    META = meta
-    meta = INFO if meta['action'] == 'getinfo' else (EXECUTED if meta['finished'] else EXECUTING)
-    return meta
+def generate_fieldnames():
+    return []
 
 
-def transaction(i=sys.stdin, o=sys.stdout):
-    meta, body = read(i)
-    meta, body = getinfo(meta), ([] if meta['action'] == 'getinfo' else execute(body))
-    write(o, meta, body)
-    return 'finished' in meta and meta['finished']
+def generate_rows():
+    collection = [{}]
+    for event in collection:
+        yield event
 
 
-def main():
-    while not transaction():
-        pass
+def inspect_fieldnames(fields):
+    global FIELD_ORDER
+    for field in fields:
+        if not field.startswith('__mv_'):
+            FIELD_ORDER.append(field)
 
 
-# From here should be all you have to worry about for all the behavior...
-
-"""  Here are the GetInfo options I could glean from code analysis... (change the INFO constant accordingly)
-                    eventing         reporting        streaming
-distributed         False            False            True*|False
-generating          True|False*      True|False*      True|False*
-maxinputs           < limits.conf    < limits.conf    < limits.conf
-required_fields     ['fn', ...]      ['fn', ...]      ['fn', ...]
-requires_preop      N/A              N/A              True|False*
-run_in_preview      N/A              True*|False      N/A
-streaming_preop     N/A              N/A              '{SPL}'
-type                'events'         'reporting'      'streaming'
-*-default  """
-INFO = {"type": "streaming", 'distributed': True}  # TODO: A lot of permutations here. This needs a lot of testing
+def alter_rows(table, writer=None):
+    results = []
+    for row in table:
+        row = mv_row_handler(row)
+        
+        if writer:
+            writer.writerow(row)
+        else:
+            results.append(mv_row_handler(row, False))
+    return results
 
 
-def execute(body):
-    """Handles manipulating the body
-    :param body: [OrderedDict(...), ...]
-    :return: [OrderedDict(...), ...]"""
-    global BODY   # Not necessary unless you want to wait for META['finished'] to handle all events
-    BODY += body  # ...but to so that, make sure to change META['finished'] to False while sending non-final chunks
-    return body   # (This example just sends the chunk right back after converting to and from runtime objects)
-# ...to here
-
-
-main()  # TODO: Now that we've "cracked the code" behind SCP2, just translate it into something fast like Go
+try:
+    main(sys.stdin, sys.stdout)
+except Exception:
+    import traceback
+    print(traceback.format_exc(), file=sys.stderr)
